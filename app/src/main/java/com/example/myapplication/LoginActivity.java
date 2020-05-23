d1c50cdcb720ae9705784f86900ecf8382ecb6af
arjun anand <arjunluam0732@gmail.com>
2020-05-23 11:55:56 +0530

    database created

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -2,13 +2,50 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText email;
+    EditText password;
+    Button login;
+    private FirebaseAuth mauth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.mail);
+        password = findViewById(R.id.pass);
+        login = findViewById(R.id.login);
+        mauth = FirebaseAuth.getInstance();
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user_mail = email.getText().toString();
+                String user_pass = password.getText().toString();
+                loginUser(user_mail,user_pass);
+            }
+        });
+
+    }
+    private void loginUser(String email,String password){
+        mauth.signInWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(LoginActivity.this,"Login Successfull",Toast.LENGTH_LONG).show();
+                startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,29 +6,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
-    private Button register;
-    private Button login;
-
+        Button signout;
+        private FirebaseAuth mauth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        register=findViewById(R.id.register);
-        login=findViewById(R.id.login);
 
-        register.setOnClickListener(new View.OnClickListener() {
+        signout=findViewById(R.id.out);
+        signout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(MainActivity.this,"Signed out sucessfully",Toast.LENGTH_LONG).show();
                 startActivity(new Intent(MainActivity.this,RegisterActivity.class));
             }
         });
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,LoginActivity.class));
-            }
-        });
+        HashMap<String,Object>map=new HashMap<>();
+        map.put("Name","Email");
+        map.put("Email","arjunluam0732@gmail.com");
+        FirebaseDatabase.getInstance().getReference().child("Arjun").updateChildren(map);
     }
 }
